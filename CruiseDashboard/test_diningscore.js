68b5bf5704d526bf38bcb3ab027562b112d8782a
Steves <steves@cobalttechllc.com>
2026-02-27 19:28:31 -0500

    Clean up project: remove 20 debug PNGs, temp files, old PlaywrightTest, publish_old, add root .gitignore

--- CruiseDashboard/test_diningscore.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const code = fs.readFileSync('wwwroot/js/app.js', 'utf8');
-// Strip out DOM stuff to just test getDynamicDiningScore
-const funcCode = code.match(/function getDynamicDiningScore[\s\S]*?return 0;\r?\n}/)[0];
-eval(funcCode);
-console.log('Result:', getDynamicDiningScore({ cruiseLine: 'Disney', nights: 7, packageDiningScore: 90, mainDiningScore: 70 }, 'package'));
-console.log('Done!');
